b17118b1e986c4cd6b9f0e926d21b659cae528dc
vintharas <jaime.gg83@gmail.com>
2011-01-28 05:26:20 -0800

    right before proxy project

--- koans/about_proxy_object_project.rb
+++ koans/about_proxy_object_project.rb
@@ -18,7 +18,11 @@ class Proxy
     # ADD MORE CODE HERE
   end
 
-  # WRITE CODE HERE
+  def method_missing(method_name, *args, &block)
+    @object.send(method_name, args)
+    @lastCall = method_name
+    @lastArg = Array.new.push(args)
+  end
 end
 
 # The proxy object should pass the following Koan:
